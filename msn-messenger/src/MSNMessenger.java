7012bc49fe54b9fca745c071b044a7d46f8f4137
JoaoAlexandre <alexandremjoao@gmail.com>
2024-05-20 16:31:16 -0300

    feat: "add ferramentas de polimorfismo"

--- msn-messenger/src/MSNMessenger.java
+++ msn-messenger/src/MSNMessenger.java
@@ -1,6 +1,7 @@
 public class MSNMessenger extends ServicoMensagemInstantanea {
     //herda os recursos de ServicoMensagemInstanea com o 'EXTENDS'
     public void enviarMensagem(){
+        validarConexaoInternet();
         System.out.println("Enviando Mensagens via MSN");
     }
     public void receberMensagem(){
--- msn-messenger/src/Telegram.java
+++ msn-messenger/src/Telegram.java
@@ -1,6 +1,7 @@
 public class Telegram extends ServicoMensagemInstantanea {
       //herda os recursos de ServicoMensagemInstanea com o 'EXTENDS'
       public void enviarMensagem(){
+            validarConexaoInternet();
             System.out.println("Enviando Mensagens via TELEGRAM");
         }
         public void receberMensagem(){
